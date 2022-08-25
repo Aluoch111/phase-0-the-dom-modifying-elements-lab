179259381e98777aaf6d2e8f0654c99c0c2e0ff0
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-08-25 12:26:26 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mains = document.getElementById('main');
+mains.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Grace is the champion'
+
+document.body.append(newHeader)
